2b830b03f86799547a1b6c0d8f2238bb5fc11e7a
TranOlivier <37579773+TranOlivier@users.noreply.github.com>
2018-05-23 14:52:55 +0200

    Update hosts.pp

--- site/profile/manifests/hosts.pp
+++ site/profile/manifests/hosts.pp
@@ -2,12 +2,12 @@ class profile::hosts {
   host { 'client1':
     ensure       => present,
     host_aliases => ['client-test2','client-dev2'],
-    ip           => 10.94.69.28,
+    ip           => '10.94.69.28',
   }
   
   host { 'client2':
     ensure       => present,
     host_aliases => ['client-test3','client-dev3'],
-    ip           => 10.94.69.37,
+    ip           => '10.94.69.37',
   }
 }
