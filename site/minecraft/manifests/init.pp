1dcc5149ac70f4369c7bc8ca9216c68ac4e8e9fc
TranOlivier <37579773+TranOlivier@users.noreply.github.com>
2018-05-24 11:53:14 +0200

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,5 +1,6 @@
 class minecraft {
-
+  include wget
+  
   file { '/opt/minecraft':
     ensure => directory,
   }
@@ -13,13 +14,16 @@ class minecraft {
     ensure => installed,
   }
   
-  file { '/opt/minecraft/server.jar':
-    ensure => file,
-    source => 'puppet:///modules/minecraft/server.jar',
+  wget::fetch { "download server.jar":
+    source      => 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar',
+    destination => '/opt/minecraft/',
+    timeout     => 0,
+    verbose     => false,
   }
   
   file { '/etc/systemd/system/minecraft.service':
     ensure => file,
+    source => 'puppet:///modules/minecraft/minecraft.service',
     content => "[Unit]\n Description=server minecraft\n Wants=network.target\n After=network.target\n[Service]\n WorkingDirectory=/opt/minecraft/\n ExecStart=/usr/bin/java -Xmx512M -Xms32M -jar server.jar nogui\n[Install]\n WantedBy=multi-user.target",
   }
   
