d6632c9bf6acd200eedacd47cc4856aad1f1accb
TranOlivier <37579773+TranOlivier@users.noreply.github.com>
2018-05-24 11:48:15 +0200

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,12 +1,11 @@
 class minecraft {
 
   file { '/opt/minecraft':
-    ensure => present,
     ensure => directory,
   }
   
   file { '/opt/minecraft/eula.txt':
-    ensure => present,
+    ensure => file,
     content => "eula=true",
   }
   
@@ -15,12 +14,12 @@ class minecraft {
   }
   
   file { '/opt/minecraft/server.jar':
-    ensure => present,
-    source => 'http://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar',
+    ensure => file,
+    source => 'puppet:///modules/minecraft/server.jar',
   }
   
   file { '/etc/systemd/system/minecraft.service':
-    ensure => present,
+    ensure => file,
     content => "[Unit]\n Description=server minecraft\n Wants=network.target\n After=network.target\n[Service]\n WorkingDirectory=/opt/minecraft/\n ExecStart=/usr/bin/java -Xmx512M -Xms32M -jar server.jar nogui\n[Install]\n WantedBy=multi-user.target",
   }
   
