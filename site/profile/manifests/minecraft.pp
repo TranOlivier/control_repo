de64f255d1bf230ab57bf8e9f47748c18787f0f7
TranOlivier <37579773+TranOlivier@users.noreply.github.com>
2018-05-24 10:49:27 +0200

    Update minecraft.pp

--- site/profile/manifests/minecraft.pp
+++ site/profile/manifests/minecraft.pp
@@ -1,2 +1,14 @@
 class profile::minecraft_server {
+  file { '/opt/minecraft':
+    ensure => present,
+    ensure => directory,
+  }
+  
+  file { '/opt/minecraft/eula.txt':
+    ensure => present,
+    content => "eula=true",
+  }
+  
+  
+    
 }
